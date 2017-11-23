fd78953756b8d7d2768c756ad7b84a83ee016377
ferdinand89 <ferdinand@koenigonline.eu>
2017-11-23 11:31:43 +0100

    Added Flytime DayPicker

---
+++ flytime-ios/FlytimeViewController.swift
@@ -0,0 +1,39 @@
+//
+//  FlytimeViewController.swift
+//  flytime-ios
+//
+//  Created by KOENIG on 23.11.17.
+//  Copyright © 2017 KOENIG. All rights reserved.
+//
+
+import UIKit
+
+class FlytimeViewController: UIViewController {
+    @IBOutlet weak var daySegmentedOutlet: UISegmentedControl!
+    
+    @IBAction func daySegmentedAction(_ sender: Any) {
+        NSLog("%1d", daySegmentedOutlet.selectedSegmentIndex)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

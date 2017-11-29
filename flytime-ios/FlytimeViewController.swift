6aa5e052ebd2f3116fedef88b54d7271dbdbb2e6
ferdinand89 <ferdinand@koenigonline.eu>
2017-11-29 13:29:19 +0100

    clean up in flytimeviewController

--- flytime-ios/FlytimeViewController.swift
+++ flytime-ios/FlytimeViewController.swift
@@ -9,11 +9,9 @@
 import UIKit
 import Charts
 class FlytimeViewController: UIViewController {
-    var timesTodayHour = [0]
-    var timesTomorrowHour = [0]
-    var timesDays: [Int]!
-    var wind: [Double]!
-    var temprature: [Double]!
+    var times = [0]
+    var wind = [0.0]
+    var temprature = [0.0]
     let datahandler = DataHandler()
     var weatherData: WeatherData!
     @IBOutlet weak var daySegmentedOutlet: UISegmentedControl!
@@ -23,52 +21,43 @@ class FlytimeViewController: UIViewController {
     @IBAction func daySegmentedAction(_ sender: Any) {
         NSLog("selectes Segment = %1d", daySegmentedOutlet.selectedSegmentIndex)
         if daySegmentedOutlet.selectedSegmentIndex == 2 {
-            timesDays.removeAll()
-            wind.removeAll()
-            temprature.removeAll()
+            clearWeatherData()
             for days in weatherData.daily.data {
                 if days.time != nil{
-                    print(days.time)
-                    print(days.temperatureMax)
-                    print(days.windSpeed)
-                    timesDays.append(days.time)
+                    times.append(days.time)
                     temprature.append(days.temperatureMax)
                     wind.append(days.windSpeed)
                 }
                 
             }
-            setChart(dataPoints: timesDays, valuesTemp: temprature, valuesWind: wind)
-            lineChartView.xAxis.setLabelCount(timesDays.count, force: true)
+            setChart(dataPoints: times, valuesTemp: temprature, valuesWind: wind)
+            lineChartView.xAxis.setLabelCount(times.count, force: true)
             lineChartView.xAxis.valueFormatter = DateValueFormatterDay()
             lineChartView.notifyDataSetChanged()
         }else if daySegmentedOutlet.selectedSegmentIndex == 1 {
-            timesTomorrowHour.removeAll()
-            wind.removeAll()
-            temprature.removeAll()
+            clearWeatherData()
             for hours in weatherData.hourly.data {
                 if hours.time >= datahandler.sunriseTimeTomorrow && hours.time <= datahandler.sunsetTimeTomorrow{
-                    timesTomorrowHour.append(hours.time)
+                    times.append(hours.time)
                     temprature.append(hours.temperature)
                     wind.append(hours.windSpeed)
                 }
             }
-            setChart(dataPoints: timesTomorrowHour, valuesTemp: temprature, valuesWind: wind)
-            lineChartView.xAxis.setLabelCount(timesTomorrowHour.count, force: true)
+            setChart(dataPoints: times, valuesTemp: temprature, valuesWind: wind)
+            lineChartView.xAxis.setLabelCount(times.count, force: true)
             lineChartView.xAxis.valueFormatter = DateValueFormatterHour()
             lineChartView.notifyDataSetChanged()
         }else{
-            timesTodayHour.removeAll()
-            wind.removeAll()
-            temprature.removeAll()
+            clearWeatherData()
             for hours in weatherData.hourly.data {
                 if hours.time >= datahandler.sunriseTimeToday && hours.time <= datahandler.sunsetTimeToday{
-                    timesTodayHour.append(hours.time)
+                    times.append(hours.time)
                     temprature.append(hours.temperature)
                     wind.append(hours.windSpeed)
                 }
             }
-            setChart(dataPoints: timesTodayHour, valuesTemp: temprature, valuesWind: wind)
-            lineChartView.xAxis.setLabelCount(timesTodayHour.count, force: true)
+            setChart(dataPoints: times, valuesTemp: temprature, valuesWind: wind)
+            lineChartView.xAxis.setLabelCount(times.count, force: true)
             lineChartView.xAxis.valueFormatter = DateValueFormatterHour()
             lineChartView.notifyDataSetChanged()
         }
@@ -81,11 +70,6 @@ class FlytimeViewController: UIViewController {
         super.viewDidLoad()
         setLineChartView()
         datahandler.getDataFromApi(latitude: 47.81009, longitude: 9.63863)
-        // will be initialized by data from server
-        timesTodayHour = [1511780400,1511784000,1511787600,1511791200]
-        timesDays = [1511780400,1511823600,1511910000,1511996400]
-        temprature = [3.83,4.77,4.93,4.08]
-        wind = [3.47,4.09,4.76,4.73]
     }
 
     override func didReceiveMemoryWarning() {
@@ -120,6 +104,11 @@ class FlytimeViewController: UIViewController {
         lineChartView.rightAxis.enabled = false
         lineChartView.backgroundColor = UIColor(red: 189/255, green: 195/255, blue: 199/255, alpha: 1)
         lineChartView.noDataText = "You need to provide data for the chart."
+    }
+    func clearWeatherData() {
+        times.removeAll()
+        wind.removeAll()
+        temprature.removeAll()
     }
     
 }
